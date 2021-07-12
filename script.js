f931f58bc74d8cf5a8bd8ecacca7903061de9bc9
Adnan Aheer <adnanaheer20@gmail.com>
2021-07-12 19:00:04 +0500

    Adding functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
- 
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // catch error here
+        console.log('Whoops, error here:', error)
+    }
+
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true
+
+    await videoElement.requestPictureInPicture()
+
+    button.disabled = false
+})
+
+selectMediaStream()
